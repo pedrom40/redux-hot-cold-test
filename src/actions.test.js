f8a0a0bd53128f64517a4ee0f1231626fdd61bb1
pedrom40 <pedro.morin1980@gmail.com>
2018-01-02 08:50:10 -0600

    added actions test that all pass

---
+++ src/actions.test.js
@@ -0,0 +1,24 @@
+import * as actions from './actions';
+
+describe('generateAuralUpdate', () => {
+  it('Should return the action', () => {
+    const action = actions.generateAuralUpdate();
+    expect(action.type).toEqual(actions.GENERATE_AURAL_UPDATE);
+  });
+});
+
+describe('restartGame', () => {
+  it('Should return the action', () => {
+    const correctAnswer = 10;
+    const action = actions.restartGame(correctAnswer);
+    expect(action.type).toEqual(actions.RESTART_GAME);
+  });
+});
+
+describe('makeGuess', () => {
+  it('Should return the action', () => {
+    const guess = 10;
+    const action = actions.makeGuess(guess);
+    expect(action.type).toEqual(actions.MAKE_GUESS);
+  });
+});
